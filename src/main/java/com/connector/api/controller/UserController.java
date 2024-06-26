01480a46b2c1f26a8a1428e4fa1b7c0cdfb14db5
DanteRoyal <dwroyal02@naver.com>
2024-06-27 08:20:48 +0900

    #3 토큰을 활용해서 회원탈퇴 기능

--- src/main/java/com/connector/api/controller/UserController.java
+++ src/main/java/com/connector/api/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.connector.api.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.connector.api.domain.user.request.UserSignRequest;
+import com.connector.api.global.auth.CurrentUser;
 import com.connector.api.service.UserService;
 
 import jakarta.validation.Valid;
@@ -27,8 +27,8 @@ public class UserController {
 		return userService.registerUser(request);
 	}
 
-	@DeleteMapping("/{userId}")
-	public void unregisterUser(@PathVariable final Long userId) {
+	@DeleteMapping()
+	public void unregisterUser(@CurrentUser final Long userId) {
 		userService.unregisterUser(userId);
 	}
 }
